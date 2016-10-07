5983805a2b1ea736f65b1efda64322a15b7b5501
DECODE101 <20chowb3@webmail.sis.edu.hk>
2016-10-07 13:42:11 +0800

    World Quiz:
    A quiz about world history

---
+++ World Quiz/js/index.js
@@ -0,0 +1,44 @@
+function quiz() {  
+  
+var question1 = prompt("What is the smallest country in the world?");
+if (question1 === "Vatican City") {
+  alert("Correct!");
+} else {
+  alert("Wrong");
+}
+ var question2 = prompt("When did world war 2 start?");
+ 
+ if (question2 === "1939") {
+   alert("Correct");
+ } else {
+   alert("Wrong");
+ }
+  
+  var question3 = prompt("In which year 9/11 happened?");
+  if (question3 === "2001") {
+    alert("Correct");
+  } else {
+    alert("Wrong");
+  }
+  
+  var question4 = prompt("What year did the titanic sink in?");
+ if (question4 === "1912") {
+   alert("Correct")
+ } else {
+   alert("Wrong");
+ }
+  
+
+var question5 = prompt("The disease that ravaged and killed a third of Europe's population in the 14th century is known as");
+
+if (question5 === "The Black Plague") {
+ alert("correct");
+} 
+  else {
+  alert("Wrong");
+} 
+
+
+  
+  
+}
